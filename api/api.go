67b29aacb4c80a8b853391adae2ecadb40a4a3fd
anquach <an.quach@the-hash.com>
2023-11-17 14:19:14 +0700

    fix business issue

--- api/api.go
+++ api/api.go
@@ -9,7 +9,7 @@ import (
 
 type Business interface {
 	ListS3StorageTree(ctx context.Context, path, tenant string) (*entity.S3ObjectTree, error)
-	RenameS3Object(ctx context.Context, object *entity.RequestFileRename) (*entity.S3ObjectTree, error)
+	RenameS3Object(ctx context.Context, object *entity.RequestFileRename) (*entity.ResponseFileRename, error)
 	DeleteS3Objects(ctx context.Context, paths []*entity.RequestObjectDelete) (*string, error)
 	UploadS3ObjectsByGenerateUrl(ctx context.Context, objects []*entity.RequestFileUpload) ([]*entity.ResponseFileUpload, error)
 	DownloadS3ObjectsByGenerateUrl(ctx context.Context, object *entity.RequestFileDownload) (*entity.ResponseFilDownload, error)
